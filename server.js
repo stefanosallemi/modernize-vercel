7fc22374e7e8d7da339412ec4ab085a4910cada7
steff <76653440+stefanosallemi@users.noreply.github.com>
2023-07-11 21:59:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
-
 require('dotenv').config()
 
 const pool = new Pool({
   user: 'default',
-  host: 'postgres://default:1KbqJZdoxP3V@ep-jolly-flower-480505-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require',
+  host: 'ep-jolly-flower-480505-pooler.eu-central-1.postgres.vercel-storage.compostgres://default:1KbqJZdoxP3V@ep-jolly-flower-480505-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require',
   database: 'verceldb',
   password: '1KbqJZdoxP3V',
   port: 5432,
