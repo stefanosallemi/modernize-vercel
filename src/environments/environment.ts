ba0f90fee24393c19f6c178ff50c80c1f4da7ab4
steff <76653440+stefanosallemi@users.noreply.github.com>
2023-07-11 21:29:01 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'postgres://default:1KbqJZdoxP3V@ep-jolly-flower-480505-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb',
+    apiUrl: 'https://modernize-vercel.vercel.app',
   };
   
